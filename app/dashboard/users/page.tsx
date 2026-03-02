8e0563a48a6dff67e323bc596ba25d8d098e1541
KolveitsStudio <kolveitsstudio@gmail.com>
2026-03-02 11:49:06 +0100

    Filter functionality + checkbox fix when filtered

--- app/dashboard/users/page.tsx
+++ app/dashboard/users/page.tsx
@@ -25,7 +25,27 @@ export default function UserPage() {
     const [open, setOpen] = useState(false)
     const [selectedUser, setSelectedUser] = useState<typeof users[number] | null>(null);
     const [selectedIds, setSelectedIds] = useState<number[]>([]);
-    
+
+    //for filtering
+    const [roleFilter, setRoleFilter] = useState<string>(""); // "" = all
+    const [query, setQuery] = useState<string>("");
+
+    const filteredUsers = users.filter((u) => {
+    const roleOk = !roleFilter || u.role === roleFilter;
+
+    const q = query.trim().toLowerCase();
+    const queryOk =
+        !q ||
+        u.name.toLowerCase().includes(q) ||
+        u.email.toLowerCase().includes(q) ||
+        String(u.number).includes(q);
+
+    return roleOk && queryOk;
+    });
+    //If i have filtered something, make sure selecting rows select visible
+    const visibleIds = filteredUsers.map((u) => u.id);
+    const allVisibleSelected = visibleIds.length > 0 && visibleIds.every((id) => selectedIds.includes(id));
+        
     return(
     <>
     <div className="max-w-[1500] mx-auto">
@@ -101,14 +121,14 @@ export default function UserPage() {
                 />
             <div className="shadow-xs pb-2 flex">
                 <div className=" whitespace-nowrap">
-                    <select className="customInput mr-2 hover:bg-black/3 duration-200 cursor-pointer">Role
+                    <select className="customInput mr-2 hover:bg-black/3 duration-200 cursor-pointer" value={roleFilter} onChange={(e) => setRoleFilter(e.target.value)}>Role
                         <option value="">Role</option>
-                        <option value="">Sales</option>
-                        <option value="">Driver</option>
-                        <option value="">IT</option>
-                        <option value="">Sales</option>
+                        <option value="Sales">Sales</option>
+                        <option value="Driver">Driver</option>
+                        <option value="IT">IT</option>
+                        <option value="Owner">Sales</option>
                     </select>
-                    <input className="customInput w-60" placeholder="Search"/>
+                    <input className="customInput w-60" placeholder="Search name, email, number" value={query} onChange={(e) => setQuery(e.target.value)}/>
                 </div>
                 <div className="flex ml-auto"> 
                     <button className="customButtonDefault hover:bg-black/3! mr-2 hidden lg:block">Export</button>
@@ -118,7 +138,7 @@ export default function UserPage() {
             <table className="w-full border-y border-black/10">
                 <thead>
                     <tr className="border-y border-black/10 bg-black/3 text-left text-textColorSecond">
-                        <th className="px-1 py-3 border-r border-black/3 font-medium whitespace-nowrap table-cell text-center"><input checked={selectedIds.length === users.length && users.length > 0} onChange={(e) => {if (e.target.checked) {setSelectedIds(users.map((u) => u.id));} else {setSelectedIds([]);}}} onClick={(e) => e.stopPropagation()} type="checkbox"  className="h-4 w-4" aria-label="Select all"/></th>
+                        <th className="px-1 py-3 border-r border-black/3 font-medium whitespace-nowrap table-cell text-center"><input checked={allVisibleSelected} onChange={(e) => {if (e.target.checked) {setSelectedIds((prev) => Array.from(new Set([...prev, ...visibleIds])));} else {setSelectedIds((prev) => prev.filter((id) => !visibleIds.includes(id)));}}} onClick={(e) => e.stopPropagation()} type="checkbox"  className="h-4 w-4" aria-label="Select all"/></th>
                         <th className="px-4 py-3 border-r border-black/3 font-medium whitespace-nowrap">Full name</th>
                         <th className="px-4 py-3 border-r hidden min-[620]:table-cell border-black/3 font-medium whitespace-nowrap">
                             <div className="flex items-center"><svg className="w-[26] h-[26] mr-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" d="M4 13h3.439a.991.991 0 0 1 .908.6 3.978 3.978 0 0 0 7.306 0 .99.99 0 0 1 .908-.6H20M4 13v6a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1v-6M4 13l2-9h12l2 9"/></svg>
@@ -154,11 +174,8 @@ export default function UserPage() {
                 </thead>
 
                 <tbody>
-                    {users.map((u) => {
-                    const rowMuted = !u.enabled ? "text-black/20" : "";
-
-                    return (
-                        <tr key={u.id} className={`cursor-pointer border-b border-black/10 hover:bg-black/2 ${rowMuted}`} onClick={()=> {setSelectedUser(u); setOpen(true)}} >
+                    {filteredUsers.map((u) => (
+                        <tr key={u.id} className={`cursor-pointer border-b border-black/10 hover:bg-black/2`} onClick={()=> {setSelectedUser(u); setOpen(true)}} >
                             <td className="text-center"><input type="checkbox" className="h-4 w-4" onChange={(e) => {setSelectedIds((prev) =>e.target.checked ? [...prev, u.id] : prev.filter((id) => id !== u.id));}} checked={selectedIds.includes(u.id)} onClick={(e) => e.stopPropagation()} aria-label={`Select booking ${u.id}`}/></td>
                             <td className="px-4 py-2 border-r border-black/3 text-textColorThird font-semibold flex whitespace-nowrap items-center"><div className="h-[28] w-[28] rounded-full overflow-hidden mr-2 bg-fuchsia-50"><Image src={u.img === "/" ? "/logo.png" : u.img} alt="pic" width={50} height={50} className="inline"/></div>{u.name}</td>
                             <td className="px-4 py-2 hidden min-[620]:table-cell border-r border-black/3 text-textColorThird font-semibold">{u.email}</td>
@@ -168,8 +185,7 @@ export default function UserPage() {
                             <td className="px-4 py-2 hidden min-[1100]:table-cell border-r border-black/3 text-textColorThird font-semibold"><div className="max-h-12 overflow-y-auto wrap-break-word pr-2">{u.lastSeen}</div></td>
                             <td className={`px-4 py-2 hidden min-[940]:table-cell text-textColorThird font-semibold${ u.enabled ? "" : "text-red-600" }`}>{u.enabled ? "Active" : "Disabled"}</td>
                         </tr>
-                    );
-                    })}
+                      ))}
                 </tbody>
                 </table>
         </div>
