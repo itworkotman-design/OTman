d9d0c5d8190a34cce880b09a2de982c99125ffa6
KolveitsStudio <kolveitsstudio@gmail.com>
2026-03-19 16:44:54 +0100

    vercel bug

--- app/dashboard/editor/home/page.tsx
+++ app/dashboard/editor/home/page.tsx
@@ -1,6 +1,8 @@
 import HomePage from "@/app/_components/site/pageComponents/HomePage";
 import { homePageContent } from "@/lib/content/HomePageContent";
 
-export default async function Page(){
-    return HomePage
+export default async function Page() {
+  return (
+    <HomePage content={homePageContent} locale="en" />
+  );
 }
