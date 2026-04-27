5a9b944ebdd09f74879accfb3074ce907ec4f304
KolveitsStudio <kolveitsstudio@gmail.com>
2026-04-27 17:24:01 +0200

    Fixed migration

--- prisma/migrations/20260427123500_add_order_extra_work_minutes/migration.sql
+++ prisma/migrations/20260427123500_add_order_extra_work_minutes/migration.sql
@@ -1,1 +1,4 @@
-ALTER TABLE "Order" ADD COLUMN "extraWorkMinutes" INTEGER NOT NULL DEFAULT 0;
+ALTER TABLE "Order" ADD COLUMN IF NOT EXISTS "extraWorkMinutes" INTEGER;
+UPDATE "Order" SET "extraWorkMinutes" = 0 WHERE "extraWorkMinutes" IS NULL;
+ALTER TABLE "Order" ALTER COLUMN "extraWorkMinutes" SET DEFAULT 0;
+ALTER TABLE "Order" ALTER COLUMN "extraWorkMinutes" SET NOT NULL;
