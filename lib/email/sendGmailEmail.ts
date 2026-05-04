e0e0caddf362b3a279d9aa11f73fea8e19e529c7
KolveitsStudio <kolveitsstudio@gmail.com>
2026-05-04 20:28:38 +0200

    ipv4 test

--- lib/email/sendGmailEmail.ts
+++ lib/email/sendGmailEmail.ts
@@ -34,7 +34,7 @@ export async function sendGmailEmail({
 
  const smtpOptions = {
    host: "smtp.gmail.com",
-   port: 465,
+   port: 587,
    secure: true,
    auth: {
      user: process.env.GMAIL_USER!,
