4fbb183116fcecd1e55ea3b60f93d9951b071815
KolveitsStudio <kolveitsstudio@gmail.com>
2026-06-29 10:08:51 +0200

    Store display fix 2

--- app/api/orders/route.ts
+++ app/api/orders/route.ts
@@ -1120,7 +1120,7 @@ export async function GET(req: Request) {
     }
 
     if (createdById) {
-      where.customerMembershipId = createdById;
+      where.createdByMembershipId = createdById;
     }
   } else if (isOrderCreator) {
     where.OR = [
